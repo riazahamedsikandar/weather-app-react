5811b57802764684608147a89199db9048b155bb
riazahamedsikandar <riazahamedsikandar@gmail.com>
2023-04-26 15:16:30 +0530

    dd

--- weatherapp/src/api.js
+++ weatherapp/src/api.js
@@ -1,11 +1,12 @@
 export const geoApiOptions = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "",// enter your rapid api key here
+      "content-type": "application/octet-stream",
+      "X-RapidAPI-Key": "901b051d0amshc2aacbc8fc0890dp1d0c68jsnf6df9b2fac1f",
       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     },
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = ""; // enter your key from openweather API
+  export const WEATHER_API_KEY = "81432f5e75cbc07e2a75d61cbb340be9"; 
